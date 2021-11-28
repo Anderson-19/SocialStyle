e3048d097925337944c17bfa8a1477ab76813176
tsandovalr <tsandovalromay@gmail.com>
2021-11-28 16:18:10 -0400

    login - user model update

---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        name:{
+            type: String,
+            require:true
+        },
+        username:{
+            type: String,
+            require:true
+        },
+        lastname:{
+            type: String,
+            require:true
+        },
+        email:{
+            type: String,
+            require:true,
+            unique:true
+        },
+        password:{
+            type: String,
+            require:true
+        },
+        location:{
+            type: String,
+            require:true
+        },
+        date:{
+            type: String,
+            require:true
+        },
+        avatar:{
+            type: String,
+            require:true
+        },
+        followers:[],
+        following: []
+    },
+    {
+        timestamps:true,
+        versionKey: false
+    }
+);
+
+const user = new mongoose.model('users', userSchema);
+
+module.exports = {
+    user
+}
